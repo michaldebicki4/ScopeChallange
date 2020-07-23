348a3ac06089b524dce542a573692734879b27b3
michaldebicki4 <michaldebicki4@gmail.com>
2020-07-23 12:42:55 +0200

    Initial commit

---
+++ src/com/javablop/X.java
@@ -0,0 +1,23 @@
+package com.javablop;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class X {
+    private int x;
+
+    public X(Scanner x) {
+        System.out.println("Enter the number : ");
+        this.x = x.nextInt();
+    }
+
+
+
+    public void x(){
+        for (int x = 1; x <=13; x++){
+            System.out.println(x + " times " + this.x + " equals " + x * this.x);
+
+
+        }
+    }
+}
